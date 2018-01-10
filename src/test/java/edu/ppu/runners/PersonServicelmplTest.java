5d8d90f992a911fea35620396026a55cf136e44b
samaamro20 <samaamro20@gmail.com>
2018-01-10 09:40:09 +0200

    my first

---
+++ src/test/java/edu/ppu/runners/PersonServicelmplTest.java
@@ -0,0 +1,19 @@
+package edu.ppu.runners;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+import edu.ppu.domain.Person;
+
+public class PersonServicelmplTest {
+
+	@Test
+	public void testgetPersons()
+	{
+		
+	}
+	
+}
